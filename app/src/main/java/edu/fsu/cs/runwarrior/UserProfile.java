ca5a3701c342dd78d0e895b68746f1bffb7c2c7e
Hannah Howard <hlh16b@my.fsu.edu>
2019-07-14 20:24:18 -0400

    Implemented user info graph
    Implemented the user profile graph extension; shows the distance run, as well as uses the users weight and average rate they were going (they input that) to calculate the amount of calories burned and graphs that.

--- app/src/main/java/edu/fsu/cs/runwarrior/UserProfile.java
+++ app/src/main/java/edu/fsu/cs/runwarrior/UserProfile.java
@@ -79,7 +79,7 @@ public class UserProfile extends Fragment {
 
     void showGraph() {
         Log.i("switch to graph", "meme");
-        //setWidget(new PastRunFragment(), "PastRuns");
+        setWidget(new GraphFragment(), "Graph");
     }
 
     void showRuns() {
